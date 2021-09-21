276b36dfa342bdaacb7cc78a8389d4f570da3c0f
WorldFS <fsworld009@gmail.com>
2021-09-21 13:26:55 +0800

    fix ts lint

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,27 +1,14 @@
-"use strict";
-/* eslint-disable */
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
-const fs = require("fs");
-const hotreload = require('electron-hot-reload');
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+import { app, BrowserWindow } from 'electron';
+
 let win;
 async function createWindow() {
-    // Create the browser window.
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            enableRemoteModule: false,
-        }
-    });
-    // Load app
-    //win.loadFile(path.join(__dirname, "dist/index.html"));
-    win.loadURL('http://localhost:8080');
-    // rest of code..
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  // Load app
+  win.loadURL('http://localhost:8080');
 }
-app.on("ready", createWindow);
-console.log('process.env.NODE_ENV ', process.env.NODE_ENV);
+
+app.on('ready', createWindow);
