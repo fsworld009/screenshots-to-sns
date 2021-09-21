47bd79ab8f3d1b830fb8094412d5b309db8100a8
WorldFS <fsworld009@gmail.com>
2021-09-21 10:43:59 +0800

    electron main page

---
+++ src/electron/main.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+const {
+  app,
+  BrowserWindow,
+  ipcMain
+} = require("electron");
+const path = require("path");
+const fs = require("fs");
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+async function createWindow() {
+
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: false, // turn off remote
+      // preload: path.join(__dirname, "preload.js") // use a preload script
+    }
+  });
+
+  // Load app
+  //win.loadFile(path.join(__dirname, "dist/index.html"));
+  win.loadURL('http://localhost:8080')
+
+  // rest of code..
+}
+
+app.on("ready", createWindow);
