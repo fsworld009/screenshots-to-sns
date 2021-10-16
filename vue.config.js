d1bd7758cd310d9aba90bca51fb83c38149938b2
WorldFS <fsworld009@gmail.com>
2021-10-16 10:18:47 +0800

    move vue to src/ui

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
 module.exports = {
   configureWebpack: {},
+  chainWebpack: (config) => {
+    config
+      .entry('app')
+      .clear()
+      .add('./src/ui/main.ts')
+      .end();
+    config.resolve.alias
+      .set('@', path.join(__dirname, './src'));
+  },
 };
